d7cdf904ca18020b9098f463bea9baf8112719f5
Jesus Sanchez <jesus.san.sev@gmail.com>
2021-07-10 20:47:25 -0400

    shortest path

--- python/data_structures/graph.py
+++ python/data_structures/graph.py
@@ -1,3 +1,4 @@
+import sys
 from dataclasses import dataclass
 from typing import Callable, TypeVar
 
@@ -16,8 +17,8 @@ def dfs(node: str, graph: dict, visited: list=None, strategy: Strategy=None):
     if strategy is not None: strategy.callback(node, graph, strategy.data)
 
     if node in graph:
-        for adj in graph[node]:
-            dfs(adj, graph, visited, strategy)
+        for (edge,_) in graph[node]:
+            dfs(edge, graph, visited, strategy)
     return
 
 def topo_sort(graph: dict):
@@ -29,22 +30,37 @@ def topo_sort(graph: dict):
     visited = []
     strategy = Strategy(build_path)
     i = N - 1
-    for node in graph:
-        if node in visited: continue
+    for edge in graph:
+        if edge in visited: continue
         strategy.data = []
-        dfs(node, graph, visited, strategy)
+        dfs(edge[0], graph, visited, strategy)
         for node in strategy.data:
             ordering[i] = node
             i -= 1
     return ordering
 
+def shortest_path(graph: dict, start, end=None):
+    topo = topo_sort(graph)
+    dist = {i:None for i in graph}
+    dist[start] = 0
+    for idx in topo:
+        if dist[idx] is None: continue
+        for (edge, w) in graph[idx]:
+            new_dist = dist[idx] + w
+            if dist[edge] is None: 
+                dist[edge] = new_dist
+            else:
+                dist[edge] = min(dist[edge], new_dist)
+    return dist
+
 
 if __name__ == "__main__":
+    # adjacency list with weights
     graph = {
-        'a': ['b', 'c', 'd'],
-        'b': ['d'],
-        'c': ['b', 'd'],
-        'd': ['e'],
+        'a': [('b', 1), ('c', 20), ('d', 5)],
+        'b': [('d', 5)],
+        'c': [('b', 2), ('d', 8)],
+        'd': [('e', 11)],
         'e': []
     }
 
@@ -52,3 +68,4 @@ if __name__ == "__main__":
 
     dfs('a', graph, strategy=strategy)
     print(topo_sort(graph))
+    print(shortest_path(graph, 'c'))
