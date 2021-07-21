cc98c6054c7e2f7f3cb4cedb588b32f0884fca1b
Jesus Sanchez <jesus.san.sev@gmail.com>
2021-07-20 20:22:54 -0400

    refactored TreeNode#from_list

--- python/data_structures/node.py
+++ python/data_structures/node.py
@@ -12,19 +12,16 @@ class TreeNode(Generic[T]):
 
     @classmethod
     def from_list(cls, nodes: List):
-        forest = [cls(val) for val in nodes]
-        count = len(forest)
-        for index, tree in enumerate(forest):
-            left_index = 2 * index + 1
-            if left_index < count:
-                node = forest[left_index]
-                if node.val is None: continue
-                tree.left = node
-            right_index = 2 * index + 2
-            if right_index < count:
-                node = forest[right_index]
-                if node.val is None: continue
-                tree.right = node
+        forest = [cls(val) if val else None for val in nodes]
+        n = len(forest)
+        for i, node in enumerate(forest):
+            if node is None: continue
+            l_i = (i * 2) + 1
+            r_i = (i * 2) + 2
+            if l_i >= n: continue
+            node.left = forest[l_i]
+            if r_i >= n: continue
+            node.right = forest[r_i]
         return forest[0]
 
 @dataclass
@@ -34,6 +31,5 @@ class GraphNode(Generic[T]):
 
 
 if __name__ == "__main__":
-    nodes = [1,10,4,3,None,7,9,12,8,6,None,None,2]
-    r = TreeNode.from_list(nodes)
-    print(r)
+    print(TreeNode.from_list([1,2,3,None,5,None,4]))
+    print(TreeNode.from_list([1,10,4,3,None,7,9,12,8,6,None,None,2]))
