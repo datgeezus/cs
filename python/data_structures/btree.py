065e938e1557dd51845067534d452572dcb75249
Jesus Sanchez <jesus.san.sev@gmail.com>
2021-08-08 18:56:43 -0400

    refactored BTree

--- python/data_structures/btree.py
+++ python/data_structures/btree.py
@@ -1,71 +1,115 @@
 from collections import deque
-from data_structures.node import TreeNode
+from dataclasses import dataclass
+from enum import Enum, unique, auto
+from typing import Callable, Generic, List, Type, TypeVar
 
-def dfs_in_order(root: TreeNode, callback=None):
-    if root is None: return
-    dfs_in_order(root.left, callback)
-    callback(root)
-    dfs_in_order(root.right, callback)
+T = TypeVar('T')
 
-def dfs_pre_order(root: TreeNode, callback=None):
-    if root is None: return
-    callback(root)
-    dfs_pre_order(root.left, callback)
-    dfs_pre_order(root.right, callback)
+@dataclass
+class BTreeNode(Generic[T]):
+    val: T = None
+    left: Type['BTreeNode'] = None
+    right: Type['BTreeNode'] = None
 
-def dfs_post_order(root: TreeNode, callback=None):
-    if root is None: return
-    dfs_post_order(root.left, callback)
-    dfs_post_order(root.right, callback)
-    callback(root)
+@dataclass
+class BTreeStrategyData:
+    data: T = None
 
-def bfs(root: TreeNode, callback=None):
-    if root is None: return
-    q = deque()
-    q.append(root)
-    while q:
-        curr = q.popleft()
-        callback(curr)
-        if curr.left: 
-            q.append(curr.left)
-        if curr.right: 
-            q.append(curr.right)
+@dataclass
+class BTreeStrategy:
+    apply: Callable[[T, BTreeStrategyData], None]
+    data: BTreeStrategyData = None
+
+@unique
+class DfsType(Enum):
+    PRE_ORDER = auto()
+    IN_ORDER = auto()
+    POST_ORDER = auto()
+
+
+@dataclass
+class BTree(Generic[T]):
+    root: BTreeNode
+
+    @classmethod
+    def from_list(cls, nodes: List[T]) -> Type['BTree']:
+        forest = [BTreeNode(val) if val else None for val in nodes]
+        n = len(forest)
+        for i, node in enumerate(forest):
+            if node is None: continue
+            l_i = (i * 2) + 1
+            r_i = (i * 2) + 2
+            if l_i >= n: continue
+            node.left = forest[l_i]
+            if r_i >= n: continue
+            node.right = forest[r_i]
+        return cls(forest[0])
+
+    def dfs(self, dfs_type: DfsType, strategy: BTreeStrategy):
+        if dfs_type == DfsType.PRE_ORDER:
+            return BTree.dfs_pre_order(self.root, strategy)
+        elif dfs_type == DfsType.IN_ORDER:
+            return BTree.dfs_in_order(self.root, strategy)
+        elif dfs_type == DfsType.POST_ORDER:
+            return BTree.dfs_post_order(self.root, strategy)
+        else:
+            raise ValueError
+
+    @staticmethod
+    def dfs_in_order(root: BTreeNode, strategy: BTreeStrategy):
+        if root is None: return
+        BTree.dfs_in_order(root.left, strategy)
+        strategy.apply(root.val, strategy.data)
+        BTree.dfs_in_order(root.right, strategy)
+
+    @staticmethod
+    def dfs_pre_order(root: BTreeNode, strategy: BTreeStrategy):
+        if root is None: return
+        strategy.apply(root.val, strategy.data)
+        BTree.dfs_pre_order(root.left, strategy)
+        BTree.dfs_pre_order(root.right, strategy)
+
+    @staticmethod
+    def dfs_post_order(root: BTreeNode, strategy: BTreeStrategy):
+        if root is None: return
+        BTree.dfs_post_order(root.left, strategy)
+        BTree.dfs_post_order(root.right, strategy)
+        strategy.apply(root.val, strategy.data)
+
+    @staticmethod
+    def bfs(root: BTreeNode, strategy: BTreeStrategy):
+        if root is None: return
+        q = deque()
+        q.append(root)
+        while q:
+            curr = q.popleft()
+            strategy.apply(curr.val, strategy.data)
+            if curr.left: 
+                q.append(curr.left)
+            if curr.right: 
+                q.append(curr.right)
 
 if __name__ == "__main__":
     """
     Building Tree
-       1
-      / \
-     2   3
+         A
         / \
-       4   5
+       B   C
+      / \ / \
+     D  E F  G
     """
-    n1 = TreeNode('A')
-    n2 = TreeNode('B')
-    n3 = TreeNode('C')
-    n4 = TreeNode('D')
-    n5 = TreeNode('E')
-    n6 = TreeNode('F')
-    n7 = TreeNode('G')
-    n8 = TreeNode('H')
-    n9 = TreeNode('I')
 
-    n6.left = n2
-    n6.right = n7
-    n2.left = n1
-    n2.right = n4
-    n4.left = n3
-    n4.right = n5
-    n7.right = n9
-    n9.left = n8
-
-    print("DFS")
-    print("in order")
-    dfs_in_order(n6, lambda n: print(n.val))
-    print("pre order")
-    dfs_pre_order(n6, lambda n: print(n.val))
-    print("post order")
-    dfs_post_order(n6, lambda n: print(n.val))
+    strategy = BTreeStrategy(lambda node_value, _: print(node_value), '')
+    btree = BTree.from_list(['A', 'B', 'C', 'D', 'E', 'F', 'G'])
 
     print("BFS")
-    bfs(n6, lambda n: print(n.val))
+    btree.bfs(btree.root, strategy)
+
+    print("DFS pre order")
+    btree.dfs(DfsType.PRE_ORDER, strategy)
+
+    print("DFS in order")
+    btree.dfs(DfsType.IN_ORDER, strategy)
+
+    print("DFS post order")
+    btree.dfs(DfsType.POST_ORDER, strategy)
