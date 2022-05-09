131bbb8e37cdc59aa74b495956538afdde0b2570
Jesus Sanchez <jesus.san.sev@gmail.com>
2022-05-08 20:02:25 -0400

    max profit

---
+++ python/neetcode/sliding_window/best_time_stock.py
@@ -0,0 +1,27 @@
+"""
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+You want to maximize your profit by choosing a single day to buy one stock and choosing a 
+different day in the future to sell that stock.
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+"""
+
+def max_profit(prices: list[int]) -> int:
+    l = 0 # buy
+    r = 1 # sell
+    max_profit = 0
+    n_prices = len(prices)
+
+    while r < n_prices:
+        # is profitable?
+        if prices[l] < prices[r]:
+            profit = prices[r] - prices[l]
+            max_profit = max(max_profit, profit)
+        else:
+            l = r
+        r += 1
+
+    return max_profit
+
+if __name__ == "__main__":
+    assert max_profit([7,1,5,3,6,4]) == 5
+    assert max_profit([7,6,4,3,1]) == 0
