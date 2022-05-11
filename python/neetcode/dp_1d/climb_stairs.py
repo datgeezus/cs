b6ce08bd2948c70dcf1fd5076a48f48e8fb9c238
Jesus Sanchez <jesus.san.sev@gmail.com>
2022-05-10 23:05:19 -0400

    climb stairs

---
+++ python/neetcode/dp_1d/climb_stairs.py
@@ -0,0 +1,23 @@
+"""
+# Problem
+You are climbing a staircase. It takes n steps to reach the top.
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+# Solution
+
+"""
+
+def climb_stairs(n: int) -> int:
+    dp = [0 for _ in range(n)]
+    for i,n in enumerate(range(n)):
+        if i == 0 or i == 1:
+            dp[i] = i+1
+            continue
+        dp[i] = dp[i-1] + dp[i-2]
+    print(dp)
+    return dp[-1]
+    
+
+if __name__ == "__main__":
+    assert climb_stairs(2) == 2
+    assert climb_stairs(3) == 3
