673d8578fd29966afffb92bd345a15d062c0603b
Jesus Sanchez <jesus.san.sev@gmail.com>
2022-05-14 13:00:16 -0400

    add description

--- python/neetcode/binary_trees/lowest_common_ancestor.py
+++ python/neetcode/binary_trees/lowest_common_ancestor.py
@@ -5,6 +5,8 @@ According to the definition of LCA on Wikipedia:
     “The lowest common ancestor is defined between two nodes p and q as the lowest node in T
     that has both p and q as descendants (where we allow a node to be a descendant of itself).”
 
+# Solution:
+Find the split in node
 
 """
 
