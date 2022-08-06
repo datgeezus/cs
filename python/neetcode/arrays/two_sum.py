60b75989f3f9ff3bb7dd52116f7373888e8ef9d4
Jesus Sanchez <jesus.san.sev@gmail.com>
2022-08-06 11:10:12 -0400

    cleanup

--- python/neetcode/arrays/two_sum.py
+++ python/neetcode/arrays/two_sum.py
@@ -1,12 +1,15 @@
-# Given an array of integers nums and an integer target,
-# return indices of the two numbers such that they add up to target.
-# You may assume that each input would have exactly one solution,
-# and you may not use the same element twice.
-# You can return the answer in any order.
+"""
+Given an array of integers nums and an integer target,
+return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution,
+and you may not use the same element twice.
+You can return the answer in any order.
+"""
 
+from collections import defaultdict
 
 def two_sum(nums: list[int], target: int) -> list[int]:
-    mem = {}
+    mem = defaultdict(int)
 
     for i, n in enumerate(nums):
         diff = target - n
