da18f4cbc801806fe706b4e0496d8ee580e55a83
Jesus Sanchez <jesus.san.sev@gmail.com>
2022-10-23 11:45:01 -0400

    extra test case

--- python/neetcode/greedy/max_sub_array.py
+++ python/neetcode/greedy/max_sub_array.py
@@ -6,6 +6,8 @@ A subarray is a contiguous part of an array.
 
 
 def max_sub_array(nums: list[int]) -> int:
+
+
     mem = [0 for _ in nums]
     for i, n in enumerate(nums):
         if i == 0:
@@ -19,3 +21,4 @@ if __name__ == "__main__":
     assert max_sub_array([-2, 1, -3, 4, -1, 2, 1, -5, 4]) == 6
     assert max_sub_array([1]) == 1
     assert max_sub_array([5, 4, -1, 7, 8]) == 23
+    assert max_sub_array([-4, -1, -2, -7, -3, -4]) == -1
